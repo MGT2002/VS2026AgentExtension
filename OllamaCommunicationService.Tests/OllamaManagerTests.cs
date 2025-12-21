4434d37dfc0b6e977e4ec8eb5e26285535815051
MGT2002 <garik.2002.mgt@gmail.com>
2025-12-22 00:08:16 +0400

    Added inital test project.

---
+++ OllamaCommunicationService.Tests/OllamaManagerTests.cs
@@ -0,0 +1,19 @@
+﻿namespace OllamaCommunicationService.Tests
+{
+    [TestClass]
+    public sealed class OllamaManagerTests
+    {
+        [TestMethod]
+        public void GetOllamaResponse()
+        {
+            // Arrange
+            var ollamaManager = new OllamaManager();
+            var prompt = "Test prompt";
+            // Act
+            var response = ollamaManager.GetOllamaResponse(prompt);
+            // Assert
+            Assert.IsNotNull(response);
+            Assert.Contains(prompt, response);
+        }
+    }
+}
